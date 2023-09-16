72fcdd942a757983e49a3760aaa0f213e5f55e6f
Cereno Deng <cereno.deng@outlook.com>
2023-09-16 17:40:34 +0800

    revise preview for video

--- components/Preview.js
+++ components/Preview.js
@@ -1,7 +1,10 @@
 export default function Preview() {
   return (
-    <div className="p-4 w-3/4">
-      Preview
+    <div className="relative p-10 w-3/4">
+      <h2>Preview</h2>
+      <div className="absolute right-0 top-0 w-[390px] h-[844px] border border-neutral-300 bg-neutral-100">
+        <video src="/demo-video.mp4"></video>
+      </div>
     </div>
   );
 }
