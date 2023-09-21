2fde568f456eed32a204730bc436c09ac046e61e
Cereno Deng <cereno.deng@outlook.com>
2023-09-21 23:18:42 +0800

    try to play different videos for different buttons

--- components/VideoPreview.js
+++ components/VideoPreview.js
@@ -16,6 +16,7 @@ export default function VideoPreview() {
   }, []);
   const [play, setPlay] = useState(true);
   const [progress, setProgress] = useState(0);
+  const [currentVideo, setCurentVideo] = useState('/demo-video-1.mp4');
 
   function onPlayClick(event) {
     if (video.current.paused || video.current.ended) {
@@ -32,16 +33,23 @@ export default function VideoPreview() {
     return date.toTimeString().slice(3, 8);
   }
 
+  function onActionClick(event) {
+    const nextVideo = event.currentTarget.dataset.video;
+    setCurentVideo(nextVideo);
+    setPlay(true);
+    setProgress(0);
+  }
+
   return (
     <div className="flex flex-col gap-y-3 absolute right-0 top-0 w-[390px] h-fit">
       <div className="relative">
-        <video ref={video} src="/demo-video.mp4" controls></video>
+        <video ref={video} src={currentVideo} controls></video>
         <div className="absolute inset-0 flex flex-col items-center justify-evenly">
           {video.current?.currentTime > 3 && video.current?.currentTime < 7 ?
             <>
-              <button className="w-48 p-4 bg-neutral-50 opacity-90 hover:bg-emerald-100">Why?</button>
-              <button className="w-48 p-4 bg-neutral-50 opacity-90 hover:bg-emerald-100">Case Study</button>
-              <button className="w-48 p-4 bg-neutral-50 opacity-90 hover:bg-emerald-100">About Us</button>
+              <button className="w-48 p-4 bg-neutral-50 opacity-90 hover:bg-emerald-100" data-video="/demo-video-2.mp4" onClick={onActionClick}>Why?</button>
+              <button className="w-48 p-4 bg-neutral-50 opacity-90 hover:bg-emerald-100" data-video="/demo-video-3.mp4" onClick={onActionClick}>Case Study</button>
+              <button className="w-48 p-4 bg-neutral-50 opacity-90 hover:bg-emerald-100" data-video="/demo-video-1.mp4" onClick={onActionClick}>About Us</button>
             </>
             : ''
           }
