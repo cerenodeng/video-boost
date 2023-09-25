4353a9e94a8a5959d58f8273056315b735bb6cba
Cereno Deng <cereno.deng@outlook.com>
2023-09-26 00:28:17 +0800

    revise input to add occupy

--- components/Input.js
+++ components/Input.js
@@ -1,16 +1,30 @@
 'use client';
 import { useEffect, useState } from 'react';
 
-export default function Input({ label, name, placeholder, returnValue }) {
+export default function Input({
+  label,
+  name,
+  placeholder,
+  occupy,
+  returnValue,
+}) {
   const [empty, setEmpty] = useState(true);
   const [value, setValue] = useState('');
   useEffect(() => {
     returnValue(value);
   }, [returnValue, value]);
 
+  function displayClass() {
+    if (typeof occupy == 'undefined') {
+      return empty ? 'hidden' : 'block';
+    } else {
+      return empty ? 'invisible' : 'visible';
+    }
+  }
+
   return (
     <div className='flex flex-col'>
-      <label className={empty ? 'invisible' : 'visible'} htmlFor={name}>
+      <label className={displayClass()} htmlFor={name}>
         {label}
       </label>
       <input
