2b340db5a71d23b5962dc4679b2c71d8580ff5e9
Cereno Deng <cereno.deng@outlook.com>
2023-10-07 20:56:26 +0800

    delete builder

--- app/admin/videos/add/page.js
+++ app/admin/videos/add/page.js
@@ -1,10 +1,16 @@
-import Builder from '@/components/Builder';
+import ButtonSetting from './ButtonSetting';
+import VideoUpload from './VideoUpload';
 import Preview from '@/components/Preview';
 
 export default function AddVideo() {
   return (
     <div className='flex'>
-      <Builder />
+      <div className='flex w-1/4 flex-col'>
+        <div className='flex flex-col gap-y-4 bg-neutral-100 p-4'>
+          <VideoUpload />
+          <ButtonSetting />
+        </div>
+      </div>
       <Preview />
     </div>
   );
