854314f2c43e1a1a4ebd57058f63983b4dcd9748
Cereno Deng <cereno.deng@outlook.com>
2023-11-08 01:08:11 +0800

    revise users api route

---
+++ app/admin/api/users/[...path]/route.js
@@ -0,0 +1,35 @@
+import * as edgedb from 'edgedb';
+
+export async function GET(request, { params }) {
+  if (params.path[0] == 'page') {
+    const page = Number(params.path[1]);
+    const client = edgedb.createClient();
+    const results = await client.query(
+      `select User {
+      id,
+      fullname := .first_name ++ ' ' ++ .last_name,
+      email
+    }`,
+    );
+    const users = results.map(({ id, fullname, email }) => [
+      id,
+      fullname,
+      email,
+    ]);
+    const totalItems = users.length;
+    const itemsPerPage = 25;
+    const totalPages = Math.ceil(users.length / itemsPerPage);
+    const currentPage = page < 0 ? 1 : page > totalPages ? totalPages : page;
+    const items = users.slice(
+      itemsPerPage * (currentPage - 1),
+      itemsPerPage * currentPage,
+    );
+    return Response.json({
+      totalItems,
+      itemsPerPage,
+      totalPages,
+      currentPage,
+      items,
+    });
+  }
+}
