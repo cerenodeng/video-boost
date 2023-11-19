5cdc81ec80c1a1a105fdcb5cefa1a0e43251540b
Cereno Deng <cereno.deng@outlook.com>
2023-11-20 00:19:08 +0800

    revise user edit for adding

--- components/UserEdit.js
+++ components/UserEdit.js
@@ -8,10 +8,11 @@ import {
 import { AlertIcon, CloseIcon, LoadingIcon } from './Icon';
 import Input from './Input';
 import { useEffect, useState } from 'react';
+import { redirect } from 'next/navigation';
 
 const queryClient = new QueryClient();
 
-function User({ id, close }) {
+function UserUpdate({ id, close }) {
   const queryClient = useQueryClient();
   queryClient.setQueryDefaults([id], { queryFn: getItems });
   const { isPending, isError, data, error } = useQuery({
@@ -165,10 +166,101 @@ function User({ id, close }) {
   );
 }
 
+function UserAdd() {
+  const [values, setValues] = useState({
+    firstName: '',
+    lastName: '',
+    email: '',
+  });
+  const [saving, setSaving] = useState(false);
+
+  function returnValue(name, id, value) {
+    setValues((values) => {
+      values[name] = value;
+      return values;
+    });
+  }
+
+  async function save(event) {
+    event.preventDefault();
+    if (!saving) {
+      setSaving(true);
+      const response = await fetch('/admin/api/users/add', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(values),
+      });
+      if (!response.ok) {
+        console.log('error to post users api');
+      } else {
+        const { id } = await response.json();
+        if (id != undefined) {
+          console.log(id);
+        }
+      }
+      setSaving(false);
+      redirect('/admin/users');
+    }
+  }
+
+  return (
+    <div className='fixed h-screen w-96 overflow-auto border-r border-neutral-200'>
+      <div className='flex flex-col gap-y-6 px-10 py-5'>
+        <div className='flex justify-between'>
+          <h3>Add User</h3>
+          <button onClick={() => location.assign('/admin/users')}>
+            <CloseIcon />
+          </button>
+        </div>
+        <Input
+          label='First Name'
+          name='firstName'
+          placeholder='First name'
+          returnValue={returnValue}
+          occupy
+        />
+        <Input
+          label='Last Name'
+          name='lastName'
+          placeholder='Last name'
+          returnValue={returnValue}
+          occupy
+        />
+        <Input
+          label='Email'
+          name='email'
+          type='email'
+          placeholder='Email'
+          returnValue={returnValue}
+          occupy
+        />
+      </div>
+      <div className='fixed -bottom-px flex w-96 justify-between bg-neutral-200 p-5 text-xl font-semibold opacity-90'>
+        <button
+          className={`${
+            saving ? 'cursor-default bg-neutral-700' : 'bg-neutral-500'
+          } flex w-28 items-center justify-center px-5 py-2.5 text-neutral-100 hover:bg-neutral-700`}
+          onClick={save}
+        >
+          {saving ? <LoadingIcon /> : 'Save'}
+        </button>
+      </div>
+    </div>
+  );
+}
+
 export default function UserEdit({ id, close }) {
   return (
-    <QueryClientProvider client={queryClient}>
-      <User id={id} close={close} />
-    </QueryClientProvider>
+    <div>
+      {id ? (
+        <QueryClientProvider client={queryClient}>
+          <UserUpdate id={id} close={close} />
+        </QueryClientProvider>
+      ) : (
+        <UserAdd />
+      )}
+    </div>
   );
 }
